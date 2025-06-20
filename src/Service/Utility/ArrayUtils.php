420d6303f7386d3da5b7bce2ead5ce6ddb3188c1
Riley Jackson <riley.jackson@torqit.ca>
2025-06-20 13:10:58 -0300

    Update get with default param and add pop function

--- src/Service/Utility/ArrayUtils.php
+++ src/Service/Utility/ArrayUtils.php
@@ -7,53 +7,68 @@ use Carbon\Carbon;
 class ArrayUtils
 {
     /** @param string|string[] $key */
-    public function get(array|string $key, ?array $array)
+    public function get(array|string $key, ?array $array, mixed $default = null)
     {
-        if(empty($array))
-        {
-            return null;
+        if (empty($array)) {
+            return $default;
         }
 
         if (is_array($key)) {
             if (empty($key)) {
-                throw new \InvalidArgumentException("You must supply a key to check for the given array (empty array of keys given)");
-            } else if (count($key) === 1) {
-                return self::get($key[0], $array);
-            }
-            else
-            {
-                return key_exists($key[0], $array) && is_array($array[$key[0]]) ?
-                    self::get(array_slice($key, 1), $array[$key[0]]) :
-                    null;
+                throw new \InvalidArgumentException(
+                    'You must supply a key to check for the given array (empty array of keys given)'
+                );
+            } elseif (count($key) === 1) {
+                return $this->get($key[0], $array);
+            } else {
+                return key_exists($key[0], $array) && is_array($array[$key[0]])
+                    ? $this->get(array_slice($key, 1), $array[$key[0]])
+                    : $default;
             }
         } else {
-            return key_exists($key, $array) ? $array[$key] : null;
+            return key_exists($key, $array) ? $array[$key] : $default;
         }
     }
 
     /** @param string|string[] $key */
-    public function getDate(array|string $key, ?array $array)
+    public function getDate(array|string $key, ?array $array, mixed $default = null)
     {
-        $val = self::get($key, $array);
-        return $val ? Carbon::parse($val) : null;
+        $val = $this->get($key, $array, $default);
+        return $val ? Carbon::parse($val) : $default;
     }
 
-    public function getInt(array|string $key, ?array $array)
+    public function getInt(array|string $key, ?array $array, mixed $default = null)
     {
-        $val = self::get($key, $array);
-        return empty($val) ? null : intval($val);
+        $val = $this->get($key, $array, $default);
+        return empty($val) ? $default : intval($val);
     }
 
-    public function getFloat(array|string $key, ?array $array)
+    public function getFloat(array|string $key, ?array $array, mixed $default = null)
     {
-        $val = self::get($key, $array);
-        return empty($val) ? null : floatval($val);
+        $val = $this->get($key, $array, $default);
+        return empty($val) ? $default : floatval($val);
+    }
+
+    /**
+     * Get first item from array matched by `fn`, remove that item from array.
+     * @param array $array of type `item`
+     * @param callable $fn with signature fn(item): bool
+     * @return array{0: mixed|null, 1: array} the first item matched by `fn` (or null) and the array with that item removed (or not).
+     */
+    public function pop(array $array, callable $fn): array
+    {
+        if (($index = $this->findIndex($fn, $array)) !== -1) {
+            $item = $array[$index];
+            $array = array_slice($array, $index, 1);
+            return [$item, $array];
+        } else {
+            return [null, $array];
+        }
     }
 
     /**
      * @param array $array of type `item`
      * @param callable $fn with signature fn(item): bool
-     * @return bool
      */
     public function any(array $array, callable $fn): bool
     {
@@ -68,7 +83,6 @@ class ArrayUtils
     /**
      * @param array $array of type `item`
      * @param callable $fn with signature fn(item, key?): bool
-     * @return bool
      */
     public function all(array $array, callable $fn): bool
     {
@@ -82,21 +96,18 @@ class ArrayUtils
 
     public function findInArray(callable $callable, ?array $array)
     {
-        $index = self::findIndex($callable, $array);
+        $index = $this->findIndex($callable, $array);
         return $index != -1 ? $array[$index] : null;
     }
 
     public function findIndex(callable $callable, ?array $array)
     {
-        if(empty($array))
-        {
+        if (empty($array)) {
             return -1;
         }
 
-        foreach($array as $index => $option)
-        {
-            if($callable($option))
-            {
+        foreach ($array as $index => $option) {
+            if ($callable($option)) {
                 return $index;
             }
         }
