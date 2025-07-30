8cfa17b8b6b57e45fde7fb7cded2efc28161342d
Riley Jackson <riley.jackson@torqit.ca>
2025-07-30 09:31:13 -0300

    Pass format and context to toDto methods

--- src/Service/Normalizer/AbstractDtoNormalizer.php
+++ src/Service/Normalizer/AbstractDtoNormalizer.php
@@ -13,20 +13,20 @@ abstract class AbstractDtoNormalizer implements NormalizerInterface
     }
 
     /** @param mixed|array $object */
-    public function toDto(mixed $object)
+    public function toDto(mixed $object, ?string $format = null, array $context = [])
     {
         if (!$object) {
             return null;
         } elseif (is_array($object)) {
             return array_map([$this, 'convertToDto'], $object);
         } else {
-            return $this->convertToDto($object);
+            return $this->convertToDto($object, $format, $context);
         }
     }
 
-    abstract protected function convertToDto(mixed $object);
+    abstract protected function convertToDto(mixed $object, ?string $format = null, array $context = []);
 
-    protected function validateObjectType(mixed $object, ?string $format = null)
+    protected function validateObjectType(mixed $object, ?string $format = null, array $context = [])
     {
         if ($object !== null && !$this->supportsNormalization($object, $format)) {
             throw new InvalidArgumentException(
@@ -40,7 +40,7 @@ abstract class AbstractDtoNormalizer implements NormalizerInterface
 
     public function normalize(mixed $object, ?string $format = null, array $context = [])
     {
-        $dto = $this->toDto($object);
+        $dto = $this->toDto($object, $format, $context);
         if (is_array($dto)) {
             return array_map(fn(object $o) => $this->objectNormalizer->normalize($o, $format, $context), $dto);
         } elseif (is_scalar($object) || is_object($object)) {
