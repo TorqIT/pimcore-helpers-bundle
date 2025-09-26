c4bfeb312d533c739e0bb5b0e9c3ba71d09f7ab4
Riley Jackson <riley.jackson@torqit.ca>
2025-09-26 14:28:16 -0300

    Make some updates to asset metadata

--- src/Service/Normalizer/AssetNormalizer.php
+++ src/Service/Normalizer/AssetNormalizer.php
@@ -12,6 +12,7 @@ use Symfony\Component\DependencyInjection\Attribute\AutoconfigureTag;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+use Torq\PimcoreHelpersBundle\Service\Utility\ArrayUtils;
 
 #[AutoconfigureTag('serializer.normalizer.torq.asset')]
 #[Autoconfigure(tags: [['name' => 'serializer.normalizer', 'priority' => -1]])]
@@ -19,6 +20,7 @@ class AssetNormalizer implements NormalizerInterface
 {
     public function __construct(
         #[Autowire(service: 'serializer.normalizer.object')] private NormalizerInterface $normalizer,
+        private ArrayUtils $utils,
         private AssetMetadataRepository $metadataRepository,
         private RequestStack $requestStack
     ) {
@@ -30,9 +32,10 @@ class AssetNormalizer implements NormalizerInterface
         ?string $format = null,
         array $context = []
     ): array|string|int|float|bool|ArrayObject|null {
-        $output = new stdClass();
+        $language = $this->utils->get(AbstractObjectNormalizer::LANGUAGE, $context, '');
         $request = $this->requestStack->getCurrentRequest();
 
+        $output = new stdClass();
         $output->id = $data->getId();
         if ($request !== null) {
             $output->fullPath = $request->getSchemeAndHttpHost() . $data->getFullPath();
@@ -43,7 +46,7 @@ class AssetNormalizer implements NormalizerInterface
         $output->mimeType = $data->getMimeType();
         $output->fileType = $data->getType();
         $output->fileSize = $data->getFileSize(formatted: true);
-        $metadata = $this->metadataRepository->getByAssetId($data->getId());
+        $metadata = $this->metadataRepository->getByAssetId($data->getId(), $language);
         $output->metadata = count($metadata) > 0 ? $this->convertAssetMetadataToObject($metadata) : null;
         return $this->normalizer->normalize($output, $format, $context);
     }
