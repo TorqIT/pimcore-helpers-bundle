5229dbf7837c13a226c24fb808e2435f913999c4
Riley Jackson <riley.jackson@torqit.ca>
2026-04-23 15:54:59 -0300

    More fixes to rsbuild.config.ts

--- studio/rsbuild.config.ts
+++ studio/rsbuild.config.ts
@@ -18,18 +18,11 @@ if (!fs.existsSync(buildPath)) {
     fs.mkdirSync(buildPath, { recursive: true });
 }
 
-const isDevServer = process.env.NODE_ENV === "development";
+const inDevelopment = process.env.NODE_ENV === "development";
 export default defineConfig({
-    mode: process.env.NODE_ENV === "production" ? "production" : "development",
+    mode: inDevelopment ? "development" : "production",
     server: {
         port: 3033,
-    },
-    dev: {
-        client: {
-            host: "localhost",
-            port: 3033,
-            protocol: "ws",
-        },
     },
     source: {
         entry: {
@@ -41,12 +34,12 @@ export default defineConfig({
     },
     output: {
         manifest: true,
-        assetPrefix: "/bundles/torqpimcorehelpers/build/" + (isDevServer ? "development" : "production"),
+        assetPrefix: "/bundles/torqpimcorehelpers/build/" + (inDevelopment ? "development" : "production"),
         cleanDistPath: true,
-        distPath: isDevServer ? devBuildPath : prodBuildPath,
+        distPath: inDevelopment ? devBuildPath : prodBuildPath,
     },
     tools: {
-        bundlerChain: (chain, { env }) => {
+        bundlerChain: (chain) => {
             chain.output.uniqueName("pimcore_helpers_bundle");
         },
     },
@@ -56,58 +49,27 @@ export default defineConfig({
         pluginModuleFederation({
             name: "pimcore_helpers_bundle",
             filename: "static/js/remoteEntry.js",
-            exposes: {
-                ".": "./src/main.ts",
-            },
             dts: false,
             remotes: {
                 "@pimcore/studio-ui-bundle": `promise new Promise(resolve => {
-          const studioUIBundleRemoteUrl = window.StudioUIBundleRemoteUrl
-          const script = document.createElement('script')
-
-          let hasScript = false;
-
-          document.querySelectorAll('script').forEach((el) => {
-            const elPathname = el.src.replace(/https?:\\/\\/[^/]+/, '')
-            const studioUIBundleRemoteUrlPathname = studioUIBundleRemoteUrl.replace(/https?:\\/\\/[^/]+/, '')
-
-            if (elPathname === studioUIBundleRemoteUrlPathname) {
-              hasScript = true;
-              return;
-            }
-          })
-
-          if (hasScript) {
-            resolve({
-              get: (request) => window['pimcore_studio_ui_bundle'].get(request),
-              init: (...arg) => {
-                try {
-                  return window['pimcore_studio_ui_bundle'].init(...arg)
-                } catch(e) {
-                  console.log('remote container already initialized')
-                }
-              }
-            })
-            return
-          }
-
-          script.src = studioUIBundleRemoteUrl
-          script.onload = () => {
-            const proxy = {
-              get: (request) => window['pimcore_studio_ui_bundle'].get(request),
-              init: (...arg) => {
-                try {
-                  return window['pimcore_studio_ui_bundle'].init(...arg)
-                } catch(e) {
-                  console.log('remote container already initialized')
-                }
-              }
-            }
-            resolve(proxy)
-          }
-          document.head.appendChild(script);
-        })
-        `,
+                    const studioUIBundleRemoteUrl = window.StudioUIBundleRemoteUrl
+                    const script = document.createElement('script')
+                    script.src = studioUIBundleRemoteUrl
+                    script.onload = () => {
+                        const proxy = {
+                            get: (request) => window['pimcore_studio_ui_bundle'].get(request),
+                            init: (...arg) => {
+                                try {
+                                    return window['pimcore_studio_ui_bundle'].init(...arg)
+                                } catch(e) {
+                                    console.log('remote container already initialized')
+                                }
+                            }
+                        }
+                      resolve(proxy)
+                    }
+                    document.head.appendChild(script);
+                })`,
             },
             shared: {
                 ...packages.dependencies,
