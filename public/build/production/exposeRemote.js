60a77265c02d6dc7b28861ec5dd7115333e742ab
lukemacausland <58705994+lukemacausland@users.noreply.github.com>
2026-09-01 17:10:47 +0000

    Automatic frontend build

---
+++ public/build/production/exposeRemote.js
@@ -0,0 +1,13 @@
+
+      if (window.pluginRemotes === undefined) {
+        window.pluginRemotes = {}
+      }
+
+      if (window.alternativePluginExportPaths === undefined) {
+        window.alternativePluginExportPaths = {}
+      }
+
+      window.pluginRemotes.pimcore_helpers_bundle = "/bundles/torqpimcorehelpers/build/production/static/js/remoteEntry.js"
+
+      
+    
